c36bc775925dfc210d6e31c44ec70369a972814d
Julian Rojas <julianrojasgeo@gmail.com>
2023-02-28 16:39:54 -0500

    Generic folder added

---
+++ src/main/java/com/dddStore/dddstore/DddstoreApplication.java
@@ -0,0 +1,13 @@
+package com.dddStore.dddstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dddStore/dddstore/DddstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dddStore.dddstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
